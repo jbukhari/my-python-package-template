9573819f9263878858a4e62c85e9099832ba056c
jbukhari <jbukhari@gmail.com>
2026-02-18 21:13:01 -0500

    updates

--- my_package/tests/test_my_module.py
+++ my_package/tests/test_my_module.py
@@ -8,3 +8,10 @@ def test_my_module(my_fixture):
     assert sub.my_attr2 == 'attr2'
     assert sub.my_method() == 1
     assert sub.my_method2() == 2
+
+def test_my_submodule():
+    from my_package.my_module.my_submodule import MyUtil
+
+    assert MyUtil()
+
+
